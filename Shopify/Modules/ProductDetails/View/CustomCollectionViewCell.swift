a10f9fa6365b54fdbfefe02929e2f94f995386e6
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-06-03 02:08:53 +0200

    working on scrollview

---
+++ Shopify/Modules/ProductDetails/View/CustomCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.swift
+//  Shopify
+//
+//  Created by Sara Talat on 03/06/2024.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var productImage: UIImageView!
+    
+}
