ecc0d41979f79eb3dc5f74782b99ebca851f46a1
haneenmedhat2 <haneenmedhat26@gmail.com>
2024-06-02 23:24:07 -0700

    order details Screen

---
+++ Shopify/Modules/Orders/View/AllOrdersCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AllOrdersCollectionViewCell.swift
+//  Shopify
+//
+//  Created by Haneen Medhat on 02/06/2024.
+//
+
+import UIKit
+
+class AllOrdersCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var totalPrice: UILabel!
+    
+    @IBOutlet weak var creationDate: UILabel!
+}
