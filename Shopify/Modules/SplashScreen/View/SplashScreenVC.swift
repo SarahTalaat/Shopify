b1f1d6c2b062e0405721598b54c741910b74573e
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-06-05 00:23:45 +0200

    splash screen done

---
+++ Shopify/Modules/SplashScreen/View/SplashScreenVC.swift
@@ -0,0 +1,43 @@
+//
+//  SplashScreenVC.swift
+//  Shopify
+//
+//  Created by Sara Talat on 05/06/2024.
+//
+
+import UIKit
+import Lottie
+
+class SplashScreenVC: UIViewController {
+
+    @IBOutlet weak var animationLottieView: AnimationView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        animationLottieView.contentMode = .scaleAspectFit
+        animationLottieView.loopMode = .loop
+        animationLottieView.play()
+        
+        
+        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(goToSignUpVC), userInfo: nil, repeats: false)
+    }
+    
+    @objc func goToSignUpVC(){
+        let sb = UIStoryboard.init(name: "Main", bundle: nil)
+        let signUpVC = sb.instantiateViewController(withIdentifier: "SignUpVC")
+        navigationController?.pushViewController(signUpVC, animated: true)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
