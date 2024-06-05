8434cd70b9c401c959e6b4c185fb52b2e6e175a2
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-06-05 21:21:25 +0200

    trying to fix navigation to signup from splash screen and onboarding

--- Shopify/Modules/Onboarding/View/OnboardingPageViewController.swift
+++ Shopify/Modules/Onboarding/View/OnboardingPageViewController.swift
@@ -19,6 +19,16 @@ class OnboardingPageViewController: UIViewController {
     
     // MARK: - Lifecycle
     
+    @IBAction func getStartedButtonTapped(_ sender: UIButton) {
+
+        // Inside the view controller you want to dismiss from
+        dismiss(animated: true, completion: nil)
+        let sb = UIStoryboard.init(name: "Main", bundle: nil)
+        let signUpVC = sb.instantiateViewController(withIdentifier: "SignUpVC")
+        navigationController?.pushViewController(signUpVC, animated: true)
+
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -30,8 +40,18 @@ class OnboardingPageViewController: UIViewController {
         myTitle.text = titleText
         myDescription.text = descriptionText
         
-        myImage.layer.cornerRadius = myImage.frame.width / 2
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        // Make the image view circular
+        myImage.contentMode = .scaleAspectFill
+        myImage.frame = CGRect(x: myImage.frame.origin.x, y: myImage.frame.origin.y, width: 306, height: 306)
+        myImage.layer.cornerRadius = myImage.frame.height / 2
         myImage.clipsToBounds = true
         
     }
+    
 }
--- Shopify/Modules/SplashScreen/View/SplashScreenVC.swift
+++ Shopify/Modules/SplashScreen/View/SplashScreenVC.swift
@@ -4,51 +4,43 @@
 //
 //  Created by Sara Talat on 05/06/2024.
 //
-
+//
 import UIKit
 import Lottie
 
 class SplashScreenVC: UIViewController {
 
     @IBOutlet weak var animationLottieView: AnimationView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+
         animationLottieView.contentMode = .scaleAspectFit
         animationLottieView.loopMode = .loop
         animationLottieView.play()
-        
-        
+
+
         Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(goToOnboardingScreen), userInfo: nil, repeats: false)
     }
-    
-//    @objc func goToSignUpVC(){
-//        let sb = UIStoryboard.init(name: "Main", bundle: nil)
-//        let signUpVC = sb.instantiateViewController(withIdentifier: "OnboardingContentViewController")
-//        navigationController?.pushViewController(signUpVC, animated: true)
-//    }
-    
+
+
     @objc func goToOnboardingScreen(){
-//        let onboardingVC = OnboardingViewController()
-//        present(onboardingVC, animated: true, completion: nil)
-        
-            let sb = UIStoryboard.init(name: "Main", bundle: nil)
-            let onboardingVC = sb.instantiateViewController(withIdentifier: "OnboardingViewController")
-            navigationController?.pushViewController(onboardingVC, animated: true)
-        
+        let onboardingVC = OnboardingViewController()
+        onboardingVC.modalPresentationStyle = .fullScreen
+        present(onboardingVC, animated: true, completion: nil)
+    }
+
+//            let sb = UIStoryboard.init(name: "Main", bundle: nil)
+//            let onboardingVC = sb.instantiateViewController(withIdentifier: "OnboardingViewController")
+//            navigationController?.pushViewController(onboardingVC, animated: true)
+
     }
 
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
-}
+
+
