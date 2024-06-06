18a3aa1424ab2aebe151bf5aa981610812fddc3c
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-06-06 19:17:24 +0200

    create signUp viewModel and viewModel protocol

---
+++ Shopify/Modules/SignUp/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpViewModel.swift
+//  Shopify
+//
+//  Created by Sara Talat on 06/06/2024.
+//
+
+import Foundation
+
+
+class SignUpViewModel {
+    
+}
---
+++ Shopify/Modules/SignUp/ViewModel/SignUpViewModelProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  SignUpViewModelProtocol.swift
+//  Shopify
+//
+//  Created by Sara Talat on 06/06/2024.
+//
+
+import Foundation
+
+class SignUpViewModelProtocol {
+    
+}
