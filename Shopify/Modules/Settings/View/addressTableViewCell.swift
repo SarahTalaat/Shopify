b340756f9a96522a5b5e535f177af3317d4b4447
MarimYacout <marim.yacout@gmail.com>
2024-06-08 02:22:49 +0300

    test

--- Shopify/Modules/Settings/View/addressTableViewCell.swift
+++ Shopify/Modules/Settings/View/addressTableViewCell.swift
@@ -1,35 +1,40 @@
+////
+////  addressTableViewCell.swift
+////  Shopify
+////
+////  Created by Marim Mohamed Mohamed Yacout on 01/06/2024.
+////
 //
-//  addressTableViewCell.swift
-//  Shopify
+//import UIKit
 //
-//  Created by Marim Mohamed Mohamed Yacout on 01/06/2024.
+//class addressTableViewCell: UITableViewCell {
 //
-
-import UIKit
-
-class addressTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.layer.borderColor = UIColor.lightGray.cgColor
-                self.layer.borderWidth = 1.0
-                self.layer.shadowColor = UIColor.black.cgColor
-                self.layer.shadowOpacity = 0.2
-                self.layer.shadowOffset = CGSize(width: 0, height: 1)
-                self.layer.shadowRadius = 4.0
-                self.layer.masksToBounds = false
-                self.layer.cornerRadius = 10.0
-                self.contentView.layer.masksToBounds = true
-    }
-
-    @IBAction func checkBtnAddress(_ sender: UIButton) {
-    }
-    @IBOutlet weak var addressDetails: UILabel!
-    @IBOutlet weak var userName: UILabel!
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
-}
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        self.layer.borderColor = UIColor.lightGray.cgColor
+//                self.layer.borderWidth = 1.0
+//                self.layer.shadowColor = UIColor.black.cgColor
+//                self.layer.shadowOpacity = 0.2
+//                self.layer.shadowOffset = CGSize(width: 0, height: 1)
+//                self.layer.shadowRadius = 4.0
+//                self.layer.masksToBounds = false
+//                self.layer.cornerRadius = 10.0
+//                self.contentView.layer.masksToBounds = true
+//    }
+//
+//    @IBAction func checkBtnAddress(_ sender: UIButton) {
+//    }
+//    
+//    @IBOutlet weak var currentAddress: UILabel!
+//    @IBOutlet weak var addressDetails: UILabel!
+//    @IBOutlet weak var userName: UILabel!
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//       
+//    }
+//    func configure(with address: Address) {
+//        userName.text = address.first_name
+//        addressDetails.text = "\(address.address1), \(address.city), \(address.country)"
+//       }
+//}
