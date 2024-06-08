9e512468b79831d742e205bc11f550acda5a72ea
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-06-08 03:25:53 +0200

    removing url param

--- Shopify/Services/NetworkService/TryAddressNetworkService.swift
+++ Shopify/Services/NetworkService/TryAddressNetworkService.swift
@@ -34,11 +34,10 @@ class TryAddressNetworkService: NetworkService {
     }
     
     func getAddresses(completion: @escaping (Swift.Result<[Address], Error>) -> Void) {
-       
-        
+     
 /*----     https://b67adf5ce29253f64d89943674815b12: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@mad44-alex-ios-team4.myshopify.com/admin/api/2022-01/customers/7493076156577/addresses.json?limit
 ----*/
-        fetchAddressData(from: url, responseType: AddressListResponse.self) { result in
+        fetchAddressData(from: , responseType: AddressListResponse.self) { result in
             switch result {
             case .success(let addressListResponse):
                 completion(.success(addressListResponse.addresses))
