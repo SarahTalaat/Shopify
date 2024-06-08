6cd0438663ed2937d6bea6a77756bdf147a3fd22
haneenmedhat2 <haneenmedhat26@gmail.com>
2024-06-08 03:34:24 -0700

    API credentials

--- Shopify/Modules/SharedFiles/NetworkUtilities.swift
+++ Shopify/Modules/SharedFiles/NetworkUtilities.swift
@@ -11,7 +11,7 @@ class NetworkUtilities{
     
     static let apiKey = "b67adf5ce29253f64d89943674815b12"
     static let shopName = "mad44-alex-ios-team4.myshopify.com"
-    static let password = ""
+    static let password = "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"
     
     static func fetchData<T: Decodable>(responseType: T.Type, endpoint: String, completion: @escaping (T?) -> Void) {
         let credentials = "\(apiKey):\(password)"
