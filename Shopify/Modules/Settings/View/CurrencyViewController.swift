a96b245604e821194e0c69ef8503624554ab7bb4
MarimYacout <marim.yacout@gmail.com>
2024-06-15 00:37:40 +0300

    Search for currency is done

--- Shopify/Modules/Settings/View/CurrencyViewController.swift
+++ Shopify/Modules/Settings/View/CurrencyViewController.swift
@@ -7,67 +7,90 @@
 
 import UIKit
 
-class CurrencyViewController: UIViewController,UITableViewDelegate, UITableViewDataSource 
+class CurrencyViewController: UIViewController,UITableViewDelegate, UITableViewDataSource,UISearchBarDelegate
 {
     
     @IBOutlet weak var currencyTableView: UITableView!
   
     var exchangeRateApiService = ExchangeRateApiService()
        var currencies: [String] = []
-    
+    var filteredCurrencies: [String] = []
     
     override func viewDidLoad() {
-            super.viewDidLoad()
+          super.viewDidLoad()
 
-            let nib = UINib(nibName: "CurrencyTableViewCell", bundle: nil)
-            currencyTableView.register(nib, forCellReuseIdentifier: "CurrencyTableViewCell")
-            self.title = "Currency"
-            currencyTableView.delegate = self
-            currencyTableView.dataSource = self
-            currencyTableView.separatorStyle = .none
-            currencyTableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+          let nib = UINib(nibName: "CurrencyTableViewCell", bundle: nil)
+          currencyTableView.register(nib, forCellReuseIdentifier: "CurrencyTableViewCell")
+          self.title = "Currency"
+          currencyTableView.delegate = self
+          currencyTableView.dataSource = self
+          searchCurrency.delegate = self
+          currencyTableView.separatorStyle = .none
+          currencyTableView.contentInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
 
-            fetchCurrencies()
-        }
+          fetchCurrencies()
+      }
 
-    func fetchCurrencies() {
-        exchangeRateApiService.getLatestRates { result in
-            switch result {
-            case .success(let response):
-                // Get all currencies from the response
-                let allCurrencies = Array(response.conversion_rates.keys)
-                var selectedCurrencies = ["USD", "EGP"]
-                while selectedCurrencies.count < 20 {
-                    if let randomCurrency = allCurrencies.randomElement(), !selectedCurrencies.contains(randomCurrency) {
-                        selectedCurrencies.append(randomCurrency)
-                    }
-                }
-                self.currencies = selectedCurrencies
-                DispatchQueue.main.async {
-                    self.currencyTableView.reloadData()
-                }
-            case .failure(let error):
-                print("Error fetching currencies: \(error.localizedDescription)")
-            }
-        }
-    }
+      func fetchCurrencies() {
+          exchangeRateApiService.getLatestRates { result in
+              switch result {
+              case .success(let response):
+                  // Get all currencies from the response
+                  let allCurrencies = Array(response.conversion_rates.keys)
+                  var selectedCurrencies = ["USD", "EGP"]
+                  while selectedCurrencies.count < 20 {
+                      if let randomCurrency = allCurrencies.randomElement(), !selectedCurrencies.contains(randomCurrency) {
+                          selectedCurrencies.append(randomCurrency)
+                      }
+                  }
+                  self.currencies = selectedCurrencies
+                  self.filteredCurrencies = selectedCurrencies
+                  DispatchQueue.main.async {
+                      self.currencyTableView.reloadData()
+                  }
+              case .failure(let error):
+                  print("Error fetching currencies: \(error.localizedDescription)")
+              }
+          }
+      }
 
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return currencies.count
-        }
+      // MARK: - Table View Methods
+      
+      func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return filteredCurrencies.count
+      }
 
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyTableViewCell", for: indexPath) as! CurrencyTableViewCell
-            cell.currency.text = currencies[indexPath.row]
-            return cell
-        }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            let selectedCurrency = currencies[indexPath.row]
-            UserDefaults.standard.set(selectedCurrency, forKey: "selectedCurrency")
-            navigationController?.popViewController(animated: true)
-        }
+      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = tableView.dequeueReusableCell(withIdentifier: "CurrencyTableViewCell", for: indexPath) as! CurrencyTableViewCell
+          cell.currency.text = filteredCurrencies[indexPath.row]
+          return cell
+      }
+      
+      func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+          let selectedCurrency = filteredCurrencies[indexPath.row]
+          UserDefaults.standard.set(selectedCurrency, forKey: "selectedCurrency")
+          navigationController?.popViewController(animated: true)
+      }
+      
+      // MARK: - Search Bar Delegate Methods
+      
+      func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+          if searchText.isEmpty {
+              filteredCurrencies = currencies
+          } else {
+              filteredCurrencies = currencies.filter { $0.localizedCaseInsensitiveContains(searchText) }
+          }
+          currencyTableView.reloadData()
+      }
+      
+      func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+          searchBar.text = ""
+          searchBar.resignFirstResponder()
+          filteredCurrencies = currencies
+          currencyTableView.reloadData()
+      }
     
-    
+    @IBOutlet weak var searchCurrency: UISearchBar!
     @IBOutlet weak var USDView: UIView!
     
     
