8a54df4fcb671373aed4bc3b643cf491f863392d
haneenmedhat2 <haneenmedhat26@gmail.com>
2024-06-20 20:30:30 -0700

    Search bar ui updates

--- Shopify/Services/NetworkService/DraftOrderNetworkService.swift
+++ Shopify/Services/NetworkService/DraftOrderNetworkService.swift
@@ -7,29 +7,39 @@
 
 import Foundation
 import Alamofire
+
 class DraftOrderNetworkService {
+    
+    let draftOrderId = UserDefaults.standard.string(forKey: Constants.shoppingCartId)
+    
     func fetchDraftOrders(completion: @escaping (Result<OneDraftOrderResponse, Error>) -> Void) {
-        let url = "https://b67adf5ce29253f64d89943674815b12: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@mad44-alex-ios-team4.myshopify.com/admin/api/2022-01/draft_orders/1034516594849.json"
-        Alamofire.request(url).responseData { response in
-            switch response.result {
-            case .success(let data):
-                print("Response data: \(String(data: data, encoding: .utf8) ?? "No data")")
-                do {
-                    let decoder = JSONDecoder()
-                    if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                       let error = jsonResponse["errors"] as? String {
-                        completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: error])))
-                    } else {
-                        let draftOrderResponse = try decoder.decode(OneDraftOrderResponse.self, from: data)
-                        completion(.success(draftOrderResponse))
+        print("mmm:\(draftOrderId)")
+        // Check if draftOrderId is not nil and try to remove "Optional(" and ")" from the string
+        if let draftOrderId = draftOrderId?.replacingOccurrences(of: "Optional(", with: "").replacingOccurrences(of: ")", with: ""),
+           let unwrappedDraftOrderId = Int(draftOrderId) {
+            print("Unwrapped draftOrderId: \(unwrappedDraftOrderId)")
+            let url = "https://b67adf5ce29253f64d89943674815b12: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@mad44-alex-ios-team4.myshopify.com/admin/api/2022-01/draft_orders/\(unwrappedDraftOrderId).json"
+            Alamofire.request(url).responseData { response in
+                switch response.result {
+                case .success(let data):
+                    print("Response data: \(String(data: data, encoding: .utf8) ?? "No data")")
+                    do {
+                        let decoder = JSONDecoder()
+                        if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                           let error = jsonResponse["errors"] as? String {
+                            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: error])))
+                        } else {
+                            let draftOrderResponse = try decoder.decode(OneDraftOrderResponse.self, from: data)
+                            completion(.success(draftOrderResponse))
+                        }
+                    } catch {
+                        print("Decoding error: \(error)")
+                        completion(.failure(error))
                     }
-                } catch {
-                    print("Decoding error: \(error)")
+                case .failure(let error):
+                    print("Network request error: \(error)")
                     completion(.failure(error))
                 }
-            case .failure(let error):
-                print("Network request error: \(error)")
-                completion(.failure(error))
             }
         }
     }
@@ -40,8 +50,8 @@ class DraftOrderNetworkService {
                return
            }
 
-           let url = "https://b67adf5ce29253f64d89943674815b12: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@mad44-alex-ios-team4.myshopify.com/admin/api/2022-01/draft_orders/\(draftOrderId).json"
-           
+        let url = "https://b67adf5ce29253f64d89943674815b12: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@mad44-alex-ios-team4.myshopify.com/admin/api/2022-01/draft_orders/\(draftOrderId).json"
+        
            let parameters: [String: Any] = [
                "draft_order": [
                    "line_items": draftOrder.draftOrder?.lineItems.map { [
