b4b26faccecb9f5e1798ee61da56be6c57c55c75
MarimYacout <marim.yacout@gmail.com>
2024-06-24 04:35:49 +0300

    enhance currency screen ui

--- Shopify/Modules/Settings/View/CurrencyTableViewCell.swift
+++ Shopify/Modules/Settings/View/CurrencyTableViewCell.swift
@@ -11,19 +11,23 @@ class CurrencyTableViewCell: UITableViewCell {
 
     @IBOutlet weak var checkBtn: UIButton!
     @IBOutlet weak var currency: UILabel!
-    
-    
+    @IBOutlet weak var currencyView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         contentView.layer.borderWidth = 1.0
-               contentView.layer.borderColor = UIColor.lightGray.cgColor
-               contentView.layer.shadowColor = UIColor.black.cgColor
-               contentView.layer.shadowOpacity = 0.5
-               contentView.layer.shadowOffset = CGSize(width: 0, height: 2)
-               contentView.layer.shadowRadius = 2.0
-               contentView.layer.masksToBounds = false
-               contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0))
+                contentView.layer.borderColor = UIColor.lightGray.cgColor
+                contentView.layer.shadowColor = UIColor.black.cgColor
+                contentView.layer.shadowOpacity = 0.5
+                contentView.layer.shadowOffset = CGSize(width: 0, height: 2)
+                contentView.layer.shadowRadius = 2.0
+                contentView.layer.masksToBounds = false
+                contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 10, bottom: 10, right: 10))
+                
+                let paddingView = UIView(frame: contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 10, bottom: 10, right: 10)))
+                paddingView.backgroundColor = .white
+                contentView.addSubview(paddingView)
+                contentView.sendSubviewToBack(paddingView)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
